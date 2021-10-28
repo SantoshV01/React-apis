f46ec46f70b3bf730e7795758726170f2148a968
Santhosh <santhvenkat2601@gmail.com>
2021-10-28 17:03:35 +0530

    'changes'

--- src/Tours.js
+++ src/Tours.js
@@ -4,21 +4,23 @@ function Tours() {
   const data = [
     {
       title: "Bests of new york in 7 days",
-      image: "",
+      image:
+        "https://images.unsplash.com/photo-1506606401543-2e73709cebb4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmV3JTIweW9yayUyMG5pZ2h0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
       price: 1995,
       description:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ad maxime voluptatum quae repudiandae cumque laboriosam asperiores! Exercitationem, recusandae soluta. Culpa eius consectetur alias quo! Quas eos exercitationem explicabo veritatis!",
     },
     {
       title: "Bests of london in 7 days",
-      image: "",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRQ23QiYnixrx6Xe6JfdruS9pq5NjK4q33wA&usqp=CAU",
       price: 2095,
       description:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ad maxime voluptatum quae repudiandae cumque laboriosam asperiores! Exercitationem, recusandae soluta. Culpa eius consectetur alias quo! Quas eos exercitationem explicabo veritatis!",
     },
     {
       title: "Bests of paris in 7 days",
-      image: "",
+      image: "https://cdn.wallpapersafari.com/15/5/RQF2z0.jpg",
       price: 1695,
       description:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ad maxime voluptatum quae repudiandae cumque laboriosam asperiores! Exercitationem, recusandae soluta. Culpa eius consectetur alias quo! Quas eos exercitationem explicabo veritatis!",
